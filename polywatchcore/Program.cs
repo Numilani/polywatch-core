624b3f2824bbbdd083fb1b85012e556add90bb87
numilani <numilani@protonmail.com>
2025-11-23 00:20:39 -0500

    add quartz

--- polywatchcore/Program.cs
+++ polywatchcore/Program.cs
@@ -1,39 +1,59 @@
 using Microsoft.EntityFrameworkCore;
 using polywatchcore.Services;
+using Quartz;
+using Serilog;
 
 namespace polywatchcore;
 
 public class Program
 {
-    public static void Main(string[] args)
+  public static async Task Main(string[] args)
+  {
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+
+    Log.Logger = new LoggerConfiguration()
+      .Enrich.FromLogContext()
+      .WriteTo.Console()
+      .WriteTo.SQLite("Data Source=polywatch-core.db")
+      .CreateLogger();
+
+    builder.Services.AddSerilog();
+
+    builder.Services.AddQuartz();
+    builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+
+    builder.Services.AddControllers();
+    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    builder.Services.AddOpenApi();
+
+    builder.Services.AddDbContext<AppDbContext>(cfg => cfg.UseSqlite("Data Source=polywatch-core.db"));
+
+    builder.Services.AddScoped<GDELTNewsService>();
+
+    var app = builder.Build();
+
+    await DoJobSetupAsync(await app.Services.GetRequiredService<ISchedulerFactory>().GetScheduler());
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
-
-        builder.Services.AddDbContext<AppDbContext>(cfg => cfg.UseSqlite("Data Source=polywatch-core.db"));
-
-        builder.Services.AddScoped<GDELTNewsService>();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.MapOpenApi();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-
-        app.MapControllers();
-
-        app.Run();
+      app.MapOpenApi();
     }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+
+    app.MapControllers();
+
+    app.Run();
+  }
+
+  private static async Task DoJobSetupAsync(IScheduler scheduler)
+  {
+
+  }
 }
